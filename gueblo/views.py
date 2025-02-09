4513e8f778d0238217d2bd837e949a557e9f43e8
duascaras <gustavodasilvapereira14@gmail.com>
2025-02-09 19:20:57 -0300

    Added URLs, views, templates and static files

--- gueblo/views.py
+++ gueblo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(request, "gueblo/post/list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, "gueblo/post/detail.html", {"post": post})
